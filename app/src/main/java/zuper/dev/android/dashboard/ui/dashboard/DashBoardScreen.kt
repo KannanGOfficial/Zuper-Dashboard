c7897b01ab0dac910c5f1f40ae7d88ec6da6cf22
KannanGOfficial <kannang143592@gmail.com>
2024-02-20 19:16:20 +0530

    Scroll is added...

--- app/src/main/java/zuper/dev/android/dashboard/ui/dashboard/DashBoardScreen.kt
+++ app/src/main/java/zuper/dev/android/dashboard/ui/dashboard/DashBoardScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -53,7 +55,9 @@ fun DashBoardScreen(
 
     Box(modifier = Modifier.fillMaxSize()) {
 
-        Column {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
+        ) {
 
             TopAppBar(title = { Text(text = "DashBoard") })
 
